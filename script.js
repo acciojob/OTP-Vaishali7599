9f5a80ab7711968548ba0a7b5e2f6a9b3c861212
Vaishali7599 <140495282+Vaishali7599@users.noreply.github.com>
2024-12-12 15:27:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+let inputs = document.querySelectorAll(".code")
+
+inputs.forEach((input, i) =>{
+	input.addEventListener("input", (e)=>{
+		let target = e.target;
+		if(target.value.match(/\d/)){
+			if(i<inputs.length-1){
+				inputs[i+1].focus();
+				target.value = target.value[0]
+			}else if(i == inputs.length-1){
+				target.value = target.value[0]
+			}
+		}else{
+			target.value = ""
+		}
+	});
+	input.addEventListener("keyup", (e)=>{
+		if(e.key === "Backspace" && i>0){
+			inputs[i-1].focus();
+		}
+	})
+	inputs[0].focus()
+})
